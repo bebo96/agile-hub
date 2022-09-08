ce81123e350b5a92b4dd1e5e7d41ea4dfa94d67b
bebo <ibrahimimran96@gmail.com>
2022-09-08 00:56:19 -0400

    Seperated comment and post-info into partials, created their helper functions, and added tests for helpers.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -21,7 +24,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+// const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
