ee6f237b82b6fc7aaa9574c9b25cbd10fd4112a5
AHamdy <abdulla.mh@hotmail.com>
2022-09-20 22:03:43 -0400

    enhanced UI / added DB tables

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,13 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const projectRoutes = require('./project-routes');
+const taskRoutes = require('./task-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/tasks', taskRoutes);
+router.use('/projects', projectRoutes);
 
 module.exports = router;
